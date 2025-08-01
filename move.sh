cfb06a4c1ee75f756a858e08d89d3e6fc9d5008d
kimyosu <kimyosu@protonmail.com>
2025-08-01 02:15:53 -0300

    mover fonts para .local/share/fonts

--- move.sh
+++ move.sh
@@ -6,5 +6,9 @@
 #!/bin/bash
 echo "instale o rofi antes :)";
 echo "movendo tema do powermenu...";
-
 sudo cp .config/rofiPowerMenu/themes/* /usr/share/rofi/themes
+
+echo "criando a pasta em ~/.local/share"
+mkdir -p ~/.local/share/fonts
+echo "movendo as fontes necessarias..."
+cp .config/rofiPowerMenu/fonts/* ~/.local/share/fonts
