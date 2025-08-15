5eae19939e0617441cdc442bf5f169a3823ec090
kimyosu <kimyosu@protonmail.com>
2025-08-15 02:22:11 -0300

    Adicionando zdk

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+function sdk
+    bash -c ". $HOME/.sdkman/bin/sdkman-init.sh && sdk $argv"
+end
+
+
 
 starship init fish | source
 zoxide init fish | source
