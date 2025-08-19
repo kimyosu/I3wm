24060940bd39ec4ef297df37f3133fcf4e25a276
kimyosu <kimyosu@protonmail.com>
2025-08-19 18:09:04 -0300

    alterando cores, icons e etc..

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+# Mata todas as barras de forma limpa
+killall polybar &
+# Espera um pouco para garantir que fecharam
+sleep 1
 
+# Roda as barras de novo
+polybar example &
+polybar second &
 
-kill polybar
-polybar &
