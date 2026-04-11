3b9c37e9994024013355586003199189b3560acf
kimyosu <kimyosu@protonmail.com>
2026-04-11 15:16:46 -0300

    removendo função

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,11 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-function spring
-    bash -c ". $HOME/.sdkman/bin/sdkman-init.sh && spring $argv"
-end
-
-
 
 starship init fish | source
 zoxide init fish | source
