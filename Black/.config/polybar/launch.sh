9647b93d9b1bf9a396cc5933a4b4cb935f205580
kimyosu <kimyosu@protonmail.com>
2026-08-03 23:55:55 -0300

    adding theme black

---
+++ Black/.config/polybar/launch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Mata todas as barras de forma limpa
+killall polybar &
+# Espera um pouco para garantir que fecharam
+sleep 5
+
+# Roda as barras de novo
+polybar example &
+polybar second &
---
+++ Nord/.config/nvim/lua/config/options.lua
@@ -0,0 +1,16 @@
+local opt = vim.opt
+
+opt.relativenumber = true -- Opcional: números relativos ajudam muito a pular linhas
+opt.number = true -- set nu
+opt.tabstop = 4 -- set tabstop=4
+opt.shiftwidth = 4 -- set shiftwidth=4
+opt.expandtab = true -- set expandtab
+opt.softtabstop = 4 -- set softtabstop=4
+opt.smartindent = true -- set smartindent
+opt.ignorecase = true -- set ignorecase
+opt.smartcase = true -- set smartcase
+opt.scrolloff = 8 -- set scrolloff=8
+opt.colorcolumn = "100" -- set colorcolumn=100
+opt.splitright = true -- set splitright
+opt.splitbelow = true -- set splitbelow
+opt.mouse = "a" -- set mouse=a
